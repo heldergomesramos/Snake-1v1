8b64b18cae39c650263638cb7fa60bb4d6fc00de
Pofinho <helderramos1414@gmail.com>
2024-09-30 11:33:41 +0100

    withCredentials: true

--- Frontend/retro-game/src/context/SignalRContext.jsx
+++ Frontend/retro-game/src/context/SignalRContext.jsx
@@ -13,7 +13,9 @@ export const SignalRProvider = ({ children }) => {
 
   useEffect(() => {
     const newConnection = new HubConnectionBuilder()
-      .withUrl(`${SERVER_BASE_URL}/lobbyHub?playerId=${playerData.playerId}`)
+      .withUrl(`${SERVER_BASE_URL}/lobbyHub?playerId=${playerData.playerId}`, {
+        withCredentials: true,
+      })
       .withAutomaticReconnect()
       .build();
 
