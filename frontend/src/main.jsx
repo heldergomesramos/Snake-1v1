e421a6a28502d7339eaaf30ead49d4a18ed62d07
Pofinho <helderramos1414@gmail.com>
2024-10-11 16:47:36 +0100

    removed if for absolute no prints

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./style.css";
 
-if (import.meta.env.MODE === "production") {
-  window.console.log = () => {};
-  window.console.warn = () => {};
-  window.console.error = () => {};
-}
+window.console.log = () => {};
+window.console.warn = () => {};
+window.console.error = () => {};
 
 createRoot(document.getElementById("root")).render(<App />);
